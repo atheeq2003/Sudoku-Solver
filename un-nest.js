41793bbce99dc61b88d96dfb4c96229ba983b1d0
atheeq_2003 <atheequrrahaman8@gmail.com>
2024-11-19 21:47:33 +0530

    unNest

---
+++ un-nest.js
@@ -0,0 +1,14 @@
+// Input: [[[[[[[{foo: "bar", fizz: "buzz"}]]]]]]]
+// Output: {foo: "bar", fizz: "buzz"}
+
+function unNest(arr) {
+    const [firstElement] = arr;
+
+    if(!Array.isArray(firstElement)) {
+        return firstElement;
+    }
+
+    return unNest(firstElement);
+}
+
+console.log(unNest([[[[[[[{foo: "bar", fizz: "buzz"}]]]]]]]));
